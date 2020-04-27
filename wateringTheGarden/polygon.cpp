ee5445bb70bcec6b0c9bd4bb399c3c27c26c9f67
Jakub Lemiesiewicz <jakub.lemiesiewicz@gmail.com>
2020-04-27 22:15:52 +0200

    polygonClass

--- wateringTheGarden/polygon.cpp
+++ wateringTheGarden/polygon.cpp
@@ -1,6 +1,31 @@
 #include "polygon.h"
+#include "point.h"
 
 Polygon::Polygon()
 {
+    this->quantity = 1;
+    this->vertices = new Point[this->quantity];
+    vertices[0] = Point();
+}
 
+Polygon::Polygon(int quantity,Point * vertices){
+    this->quantity = quantity;
+    vertices = new Point[this->quantity];
+    for(int i = 0 ; i < this->quantity ; i++){
+        this->vertices[i] = vertices[i];
+    }
+}
+
+int Polygon::getQuantity(){
+    return this->quantity;
+}
+Point*Polygon::getVertices(){
+    return this->vertices;
+}
+
+void Polygon::setCount(int quantity){
+    this->quantity = quantity;
+}
+void Polygon::setVertices(Point * vertices){
+    this->vertices = vertices;
 }
--- wateringTheGarden/polygon.h
+++ wateringTheGarden/polygon.h
@@ -1,11 +1,22 @@
 #ifndef POLYGON_H
 #define POLYGON_H
-
+#include "point.h"
 
 class Polygon
 {
+    int quantity;
+    Point * vertices;
 public:
     Polygon();
+    Polygon(int quantity,Point * vertices);
+
+    int getQuantity();
+    Point*getVertices();
+
+    void setCount(int quantity);
+    void setVertices(Point * vertices);
+
+    ~Polygon(){}
 };
 
 #endif // POLYGON_H
