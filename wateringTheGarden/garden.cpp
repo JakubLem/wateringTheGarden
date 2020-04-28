9355025a4adf982259659442d8dbb993b2e5b668
Jakub Lemiesiewicz <jakub.lemiesiewicz@gmail.com>
2020-04-28 20:30:55 +0200

    gardenClassWorking_v2

--- wateringTheGarden/garden.cpp
+++ wateringTheGarden/garden.cpp
@@ -23,17 +23,46 @@ Garden::Garden(int areasQuantity, Polygon * areas, double budget, int sprinklers
 }
 
 //getters
-int Garden::getAreasQuantity();
-Polygon * Garden::getAreas();
-double Garden::getBudget();
-int Garden::getSprinklersQuantity();
-Sprinkler * Garden::getSprinklers();
-Sprinkler Garden::getSprinkler(int i);
+int Garden::getAreasQuantity(){
+    return this->areasQuantity;
+}
+Polygon * Garden::getAreas(){
+    return this->areas;
+}
+double Garden::getBudget(){
+    return this->budget;
+}
+int Garden::getSprinklersQuantity(){
+    return this->sprinklersQuantity;
+}
+Sprinkler * Garden::getSprinklers(){
+    return this->sprinklers;
+}
+Sprinkler Garden::getSprinkler(int i){
+    if(i<this->sprinklersQuantity){
+        return this->sprinklers[i];
+    }
+    return Sprinkler();
+}
 
 //setters
-void Garden::setAreasQuantity(int areasQuantity);
-void Garden::setAreas(Polygon * areas);
-void Garden::setBudget(double budget);
-void Garden::setSprinklersQuantity(int sprinklersQuantity);
-void Garden::setSprinklers(Sprinkler * sprinklers);
-void Garden::setSprinkler(int i, Sprinkler sprinkler);
+void Garden::setAreasQuantity(int areasQuantity){
+    this->areasQuantity = areasQuantity;
+}
+void Garden::setAreas(Polygon * areas){
+    this->areas = areas;
+}
+void Garden::setBudget(double budget){
+    this->budget = budget;
+}
+void Garden::setSprinklersQuantity(int sprinklersQuantity){
+    this->sprinklersQuantity = sprinklersQuantity;
+}
+void Garden::setSprinklers(Sprinkler * sprinklers){
+    this->sprinklers = sprinklers;
+}
+void Garden::setSprinkler(int i, Sprinkler sprinkler){
+    if(i<this->sprinklersQuantity){
+        this->sprinklers[i] = sprinkler;
+    }
+}
